47217ea0fc4559d63e6455d5593a62c1082de673
shunma <shunmakajioka12@gmail.com>
2024-04-07 20:27:27 +0900

    uiの完成(仮)

--- src/app/components/contact/page.tsx
+++ src/app/components/contact/page.tsx
@@ -6,9 +6,12 @@ import React from 'react'
 import { Input } from '@/components/ui/input'
 import { useForm } from 'react-hook-form'
 import z from 'zod'
+import { Textarea } from '@/components/ui/textarea'
 
 const formShechma = z.object({
-  name: z.string().min(2,{message:'2文字以上で入力してください'}).max(10, {message: '10文字以上で入力してください'})
+  name: z.string().min(4,{message:'4文字以上で入力してください'}).max(15, {message: '15文字以下で入力してください'}),
+  email: z.string().email({message: 'メールアドレスの形式ではありません'}),
+  content: z.string().min(1,{message: '1文字以上で入力してください'})
 })
 
 type formType = z.infer<typeof formShechma>
@@ -20,17 +23,20 @@ const Contact = () => {
     resolver: zodResolver(formShechma),
     defaultValues: {
       name: "",
+      email: "",
+      content: ""
     }
   })
 
   const onSubmit = (data:formType) => {
     console.log(data)
+    form.reset()
   }
 
   return (
     <div className='container h-screen flex items-center'>
-      <div className='lg:w-[60%] w-full mx-auto'>
-        <h2 className='text-[40px] font-bold mb-[30px]'>お問い合わせフォーム</h2>
+      <div className='lg:w-[60%] w-full mx-auto '>
+        <h2 className='text-[30px] md:text-[40px] font-bold mb-[30px]'>お問い合わせフォーム</h2>
         <Form {...form}>
           <form  className='space-y-8'  onSubmit={form.handleSubmit(onSubmit)}>
             <FormField 
@@ -43,6 +49,34 @@ const Contact = () => {
                   <Input placeholder='名前を入力'{...field} />
                 </FormControl>
                 <FormDescription>名前を入力してください</FormDescription>
+                <FormMessage/>
+              </FormItem>
+             )}
+            />
+            <FormField 
+             control={form.control}
+             name = 'email'
+             render={({field}) => (
+              <FormItem>
+                <FormLabel>メールアドレス</FormLabel>
+                <FormControl>
+                  <Input placeholder='exaple@yahoo.co.jp'{...field} />
+                </FormControl>
+                <FormDescription>メールアドレスを入力してください</FormDescription>
+                <FormMessage/>
+              </FormItem>
+             )}
+            />
+            <FormField 
+             control={form.control}
+             name = 'content'
+             render={({field}) => (
+              <FormItem>
+                <FormLabel>お問い合わせ内容</FormLabel>
+                <FormControl>
+                  <Textarea  placeholder='コールセンターのアドレスが知りたい'{...field} className='resize-none h-[200px]' />
+                </FormControl>
+                <FormDescription>お問い合わせ内容を入力してください</FormDescription>
                 <FormMessage/>
               </FormItem>
              )}
